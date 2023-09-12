3c75f49875382707d3bdbf6099358c4fdf2aa187
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2023-09-12 10:35:53 +0000

    Class

--- account.cpp
+++ account.cpp
@@ -24,26 +24,55 @@ public:
     // Default constructor.
     // Initialize 'balance' to 0.
     // TODO: Implement the default constructor.
+    Account::Account (){
+    balance=0;
+    }
 
     // Parameterized constructor.
     // Initialize 'balance' with the provided 'initialBalance'.
     // TODO: Implement the parameterized constructor.
+    Account::Account(double initialBalance){
+    balance=initialBalance;
+    }
+
 
     // Getter function to return the current balance.
     // TODO: Implement the 'getBalance' function.
+    double getBalance(){
+        return balance;
+    }
 
     // Setter function to set the balance to a new value.
     // TODO: Implement the 'setBalance' function.
+    void setBalance(double newBalance){
+        balance=newBalance;
+    }
 
     // Function to deposit an amount into the account.
     // TODO: Implement the 'deposit' function.
+    void deposit(double amount)
+    {
+        balance=balance+amount;
+    }
 
     // Function to withdraw an amount from the account.
     // TODO: Implement the 'withdraw' function.
+    void withdraw(double amount){
+        if (balance>=amount){
+            balance=balance-amount;
+        }
+        else{
+            cout<< "The withdraw was unsuccesfull. Your balance is less than the amount you want to withdraw."<< endl;
+        }
+
+    }
 
     // Destructor.
     // Display a message indicating the account is being destroyed.
     // TODO: Implement the destructor.
+    ~Account(){
+        cout<<"The object created will be destroyed, and its data will be deleted."<< endl;
+    }
 };
 
 #endif // ACCOUNT_H
--- account.h
+++ account.h
@@ -46,4 +46,5 @@ public:
     ~Account();
 };
 
+
 #endif // ACCOUNT_H
